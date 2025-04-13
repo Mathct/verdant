9d33f3de45c4d2a97233f2c12cc5342517bd5c44
mathct <mathieu.chatrain@gmail.com>
2025-04-13 20:31:30 +0200

    commit

--- verdant.js
+++ verdant.js
@@ -2214,8 +2214,8 @@ showHelpModal2: function() {
     html += "<div class='tooltip_desc'>"+_('1, 3, 6, 9, 12, 16, 20, 25 points scored for unique Pet/Furniture tokens in their home.')+"</div>";
 
     html += "<br><div class='tooltip_subtitle'>"+_('Collector & Decorator')+"</div>";
-    html += "<div class='tooltip_desc'>"+_('3 points if home contains at least 1 of the 5 different plant types.')+"</div>";
-    html += "<div class='tooltip_desc'>"+_('3 points if home contains at least 1 of the 5 different room types.')+"</div>";
+    html += "<div class='tooltip_desc'>"+_('3 points if home contains all of the 5 different plant types.')+"</div>";
+    html += "<div class='tooltip_desc'>"+_('3 points if home contains all of the 5 different room types.')+"</div>";
 
     html += '</div>'
 
